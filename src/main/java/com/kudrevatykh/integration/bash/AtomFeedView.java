4202a231526fc277c044cf3484da0c298f9cc15b
Alexander Kudrevatykh <kudrevatykh@gmail.com>
2014-07-01 22:13:07 +0400

    fix atom feed

--- src/main/java/com/kudrevatykh/integration/bash/AtomFeedView.java
+++ src/main/java/com/kudrevatykh/integration/bash/AtomFeedView.java
@@ -14,14 +14,18 @@ import org.springframework.web.servlet.view.feed.AbstractAtomFeedView;
 
 import com.sun.syndication.feed.atom.Content;
 import com.sun.syndication.feed.atom.Entry;
+import com.sun.syndication.feed.atom.Feed;
 
 @Component
 public class AtomFeedView extends AbstractAtomFeedView {
+
+	private static String FEED_URL = "http://bash.im/abyssbest";
 
 	@Override
 	protected List<Entry> buildFeedEntries(Map<String, Object> model,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
+		response.setCharacterEncoding("UTF-8");
 		@SuppressWarnings("unchecked")
 		List<Map<String, Object>> rss = (List<Map<String, Object>>) model
 				.get("rss");
@@ -29,7 +33,7 @@ public class AtomFeedView extends AbstractAtomFeedView {
 		for (Map<String, Object> m : rss) {
 			Entry e = new Entry();
 			e.setPublished((Date) m.get("date"));
-			e.setId((String) m.get("id"));
+			e.setTitle((String) m.get("id"));
 			Content c = new Content();
 			c.setType(Content.HTML);
 			c.setValue((String) m.get("text"));
@@ -37,6 +41,19 @@ public class AtomFeedView extends AbstractAtomFeedView {
 			list.add(e);
 		}
 		return list;
+	}
+
+	@Override
+	protected void buildFeedMetadata(Map<String, Object> model, Feed feed,
+			HttpServletRequest request) {
+		feed.setTitle("bash abyss");
+		feed.setId(FEED_URL);
+		@SuppressWarnings("unchecked")
+		List<Map<String, Object>> rss = (List<Map<String, Object>>) model
+				.get("rss");
+		if (!rss.isEmpty()) {
+			feed.setUpdated((Date) rss.get(0).get("date"));
+		}
 	}
 
 }
